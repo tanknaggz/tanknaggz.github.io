0cadea2c9c5be0998e1b6735787ef240be350e3a
Pierluigi Palma <ppalma@istella.ai>
2020-12-24 18:51:06 +0100

    added navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,93 @@
 if ($('.typed').length) {
-    var typed_strings = $(".typed").data('typed-items');
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  var typed_strings = $(".typed").data('typed-items');
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
+
+
+var lnStickyNavigation;
+
+$(document).ready(function () {
+  applyHeader();
+  applyNavigation();
+  //applyMailTo();
+  //applyResize();
+  //checkHash();
+  //checkBrowser();
+});
+
+function applyHeader()
+{
+	$('#hero').css({ height: ($(window).height()) +'px' });
+	
+	lazyLoad($('.jumbotron'));
+}	
+
+function lazyLoad(poContainer)
+{
+	/*var lstrSource   = poContainer.attr('data-src');
+	var lstrPosition = poContainer.attr('data-position');
+
+	$('<img>').attr('src', lstrSource).load(function()
+	{
+		poContainer.css('background-image', 'url("'+ lstrSource +'")');
+		poContainer.css('background-position', lstrPosition);
+		poContainer.css('-ms-filter', '"progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\')"');
+		poContainer.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\'');
+	});*/
+}
+
+function applyNavigation()
+{
+	applyClickEvent();
+	//applyNavigationFixForPhone();
+	//applyScrollSpy();
+	applyStickyNavigation();
+}
+
+function applyStickyNavigation()
+{
+	lnStickyNavigation = $('.scroll-down').offset().top + 20;
+	
+	$(window).on('scroll', function() 
+	{  
+		stickyNavigation();  
+	});  
+	
+	stickyNavigation();
+}
+
+function stickyNavigation()
+{         
+	if($(window).scrollTop() > lnStickyNavigation) 
+	{   
+		$('body').addClass('fixed');  
+	} 
+	else 
+	{  
+		$('body').removeClass('fixed');   
+	}  
+}
+
+function applyClickEvent()
+{
+	$('a[href*=\\#]').on('click', function(e)
+	{
+		e.preventDefault();
+		
+		if( $( $.attr(this, 'href') ).length > 0 )
+		{
+			$('html, body').animate(
+			{
+				scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 400);
+		}
+		return false;
+	});
+}
